58cae8eb98b216c60054b846558104934290c18d
oxydable <gniamor>
2024-09-24 13:51:50 +0200

    Cour H3 - 24-09-2024 13:51

--- fast-API/main.py
+++ fast-API/main.py
@@ -4,6 +4,8 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+route = "romain"
+
 
 @app.get("/")
 def read_root():
@@ -13,3 +15,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/{route}")
+def read_root():
+    return {"Hello": "Tada"}
